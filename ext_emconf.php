318231589e367e2982fa6bc7459f36c0eab8dc84
Benjamin Franzke <benjaminfranzke@gmail.com>
2021-02-23 00:01:18 +0100

    Set supported TYPO3 version to v11.1.x only
    The MFA API is still internal, therefore (breaking) changes
    are to be expected in coming core releases

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.2.0-11.99.99',
+            'typo3' => '11.1.0-11.1.99',
             'php' => '7.4.0-7.4.99',
         ],
         'conflicts' => [],
