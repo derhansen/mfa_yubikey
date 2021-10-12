37b9b1dfed6e58a682213545e9ae3acee306ae27
Torben Hansen <derhansen@gmail.com>
2021-10-12 14:40:38 +0200

    [TASK] Fixed version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.4.99',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
