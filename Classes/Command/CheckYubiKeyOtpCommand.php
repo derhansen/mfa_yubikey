8075ffcc8c9567f7f2f072964d6267222f3ff684
Torben Hansen <derhansen@gmail.com>
2026-03-23 18:49:01 +0100

    [!!!][FEATURE] Added TYPO3 v14 compatibility
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Classes/Command/CheckYubiKeyOtpCommand.php
+++ Classes/Command/CheckYubiKeyOtpCommand.php
@@ -12,15 +12,16 @@ declare(strict_types=1);
 namespace Derhansen\MfaYubikey\Command;
 
 use Derhansen\MfaYubikey\Service\YubikeyAuthService;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Attribute\AsNonSchedulableCommand;
 
-/**
- * Class CheckYubiKeyOtpCommand
- */
+#[AsCommand('mfa_yubikey:checkyubikeyotp', 'Checks the given OTP against the configured YubiKey endpoints')]
+#[AsNonSchedulableCommand]
 class CheckYubiKeyOtpCommand extends Command
 {
     private YubikeyAuthService $yubikeyAuthService;
--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'uploadfolder' => 0,
     'clearCacheOnLoad' => 1,
-    'version' => '3.0.1',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '14.1.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
